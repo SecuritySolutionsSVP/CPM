51e9e87d6082781dab67456a9dc8d6b63c0c216a
Kristian Lind Kristensen <kris9894@edu.mercantec.dk>
2021-05-24 17:17:02 +0200

    Add passwords table migration

---
+++ database/migrations/2021_05_24_141032_create_passwords_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePasswordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('passwords', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('password_group_id')->constrained('password_groups');
+            $table->string('username');
+            $table->string('value');
+            $table->boolean('is_sensitive');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('passwords');
+    }
+}
