8cbbd2aaac9d7c74570c7de8d0872a51a0fcadf1
Kristian Lind Kristensen <kris9894@edu.mercantec.dk>
2021-05-25 12:02:06 +0200

    Fix class naming scheme to match table naming

--- database/migrations/2021_05_24_141759_create_user_credential_privileges_table.php
+++ database/migrations/2021_05_24_141759_create_user_credential_privileges_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUserCredentialsTable extends Migration
+class CreateUserCredentialPrivilegesTable extends Migration
 {
     /**
      * Run the migrations.
