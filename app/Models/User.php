c95217945f619224505c10003c071959b10f3a62
Kristian Lind Kristensen <kris9894@edu.mercantec.dk>
2021-05-26 18:58:51 +0200

    Make user searchable

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,11 @@ use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Scout\Searchable;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, Searchable;
 
     // gets the role of the current user by exposing prop "$role"
     public function role() {
