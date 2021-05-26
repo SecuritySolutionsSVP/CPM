59f54e91b2238379fd55aa376ddc3c035cc4b720
Kristian Lind Kristensen <kris9894@edu.mercantec.dk>
2021-05-26 11:25:33 +0200

    Add database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Credential;
+use App\Models\Group;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,29 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // generate models
+        $users = User::factory(10)->create();
+
+        $credentials = Credential::factory(10)->create();
+
+        $groups = Group::factory(10)->create();
+
+        // populate pivot tables
+        $users->each(function ($user) use ($groups, $credentials) {
+            $user->groups()->attach(
+                $groups->random(rand(1, 3))->pluck('id')->toArray()
+            );
+
+            $user->personalCredentialPrivileges()->attach(
+                $credentials->random(rand(1,3))->pluck('id')->toArray()
+            );
+        });
+
+        $groups->each(function ($group) use ($credentials) {
+            $group->credentialPrivileges()->attach(
+                $credentials->random(rand(1,3))->pluck('id')->toArray()
+            );
+        });
+
     }
 }
