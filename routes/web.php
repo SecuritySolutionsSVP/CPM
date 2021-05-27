a5a14e88dbe6bc12a9b63336218ae049b1616a65
Kristian Lind Kristensen <kris9894@edu.mercantec.dk>
2021-05-28 01:45:20 +0200

    add auth middleware to all endpoints

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| contains the 'web' middleware group. Now create something great!
 |
 */
 
@@ -25,18 +25,18 @@ Route::get('/login', [AuthController::class, 'loginView'])->name('login');
 Route::post('/login', [AuthController::class, 'authenticate']);
 Route::get('/logout', [AuthController::class, 'logout']);
 
-// Route::middleware(['auth'])->group(function() {
-    Route::get('/usergroups', [UserGroupController::class, "allUsergroupsView"]);
-    Route::get('/usergroups/{id}/passwords', [UserGroupController::class, "groupPasswordsView"]);
-    Route::get('/user/{id}/passwords', [UserGroupController::class, "myPasswordsView"]);
+Route::middleware(['auth'])->group(function() {
+    Route::get('/usergroups', [UserGroupController::class, 'allUsergroupsView']);
+    Route::get('/usergroups/{id}/passwords', [UserGroupController::class, 'groupPasswordsView']);
+    Route::get('/user/{id}/passwords', [UserGroupController::class, 'myPasswordsView']);
     
-    Route::get('/', [PasswordController::class, "notificationPasswordsView"]);
-    Route::get('/password', [PasswordController::class, "allPasswordsView"]);
+    Route::get('/', [PasswordController::class, 'notificationPasswordsView']);
+    Route::get('/password', [PasswordController::class, 'allPasswordsView']);
     
-    Route::get('/users', [UserController::class, "userView"]);
-    Route::get('/user/{id}', [UserController::class, "profileView"]);
+    Route::get('/users', [UserController::class, 'userView']);
+    Route::get('/user/{id}', [UserController::class, 'profileView']);
     
-    Route::get('/site-settings', [SiteController::class, "siteSettingsView"]);
+    Route::get('/site-settings', [SiteController::class, 'siteSettingsView']);
     
-    Route::get('/logs', [LogController::class, "logView"]);
-// });
+    Route::get('/logs', [LogController::class, 'logView']);
+});
