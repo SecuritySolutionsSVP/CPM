d4c8fa9bf195d58cefc2d785484efc880c298bc6
Kristian Lind Kristensen <kris9894@edu.mercantec.dk>
2021-06-03 08:56:55 +0200

    Remove PasswordController from using statements

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CredentialController;
 use App\Http\Controllers\UserGroupController;
 use App\Http\Controllers\GroupController;
-use App\Http\Controllers\PasswordController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\SiteController;
@@ -30,18 +29,18 @@ Route::get('/logout', [AuthController::class, 'logout']);
 
 Route::middleware(['auth'])->group(function() {
     Route::get('/usergroups', [UserGroupController::class, 'allUsergroupsView']);
-    Route::get('/usergroups/{id}/passwords', [UserGroupController::class, 'groupPasswordsView']);
-    Route::get('/user/{id}/passwords', [UserGroupController::class, 'myPasswordsView']);
+    Route::get('/usergroups/{id}/credentials', [UserGroupController::class, 'groupCredentialsView']);
+    Route::get('/user/{id}/credentials', [UserGroupController::class, 'myCredentialsView']);
     
-    Route::get('/', [CredentialController::class, "notificationPasswordsView"]);
-    Route::get('/password', [CredentialController::class, "allPasswordsView"]);
+    Route::get('/', [CredentialController::class, "notificationCredentialsView"]);
+    Route::get('/credential', [CredentialController::class, "allCredentialsView"]);
     
     Route::get('/users', [UserController::class, 'userView']);
     Route::get('/user/{id}', [UserController::class, 'profileView']);
 
     Route::get('/usergroups', [UserGroupController::class, "allUsergroupsView"]);
-    Route::get('/usergroups/{id}/passwords', [UserGroupController::class, "groupPasswordsView"])->whereNumber('id');
-    Route::get('/user/{id}/passwords', [UserGroupController::class, "myPasswordsView"])->whereNumber('id');
+    Route::get('/usergroups/{id}/credentials', [UserGroupController::class, "groupCredentialsView"])->whereNumber('id');
+    Route::get('/user/{id}/credentials', [UserGroupController::class, "myCredentialsView"])->whereNumber('id');
     
     // Group
     Route::get('/groups', [GroupController::class, "groupsView"]);
@@ -49,8 +48,8 @@ Route::middleware(['auth'])->group(function() {
     Route::put('/usergroups', [GroupController::class, "update"]);
     Route::delete('/usergroups',[GroupController::class, "delete"]);
 
-    Route::get('/', [PasswordController::class, "notificationPasswordsView"]);
-    Route::get('/password', [PasswordController::class, "allPasswordsView"]);
+    Route::get('/', [CredentialController::class, "notificationCredentialsView"]);
+    Route::get('/credential', [CredentialController::class, "allCredentialsView"]);
     
     //User
     Route::get('/users', [UserController::class, "userView"]);
@@ -65,7 +64,7 @@ Route::middleware(['auth'])->group(function() {
     Route::get('/user/{id}', [UserController::class, "profileView"])->whereNumber('id');
     Route::post('/user', [UserController::class, "create"]);
     Route::put('/user', [UserController::class, "update"]);
-    Route::put('/user/password', [UserController::class, "updatePassword"]);
+    Route::put('/user/credential', [UserController::class, "updateCredential"]);
     Route::delete('/user',[UserController::class, "delete"]);
     Route::delete('/user/force',[UserController::class, "forceDelete"]);
     
