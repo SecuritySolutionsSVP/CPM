66a8a861446e3901f72e9539de74774f1670de09
Christian Normann Sørensen <christian1dk@gmail.com>
2021-06-04 10:10:04 +0200

    user_credential_privileges Add, Read and Delete

--- app/Http/Controllers/CredentialController.php
+++ app/Http/Controllers/CredentialController.php
@@ -3,14 +3,91 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+<<<<<<< HEAD
+=======
+use Illuminate\Support\Facades\Validator;
+use App\Models\Credential;
+use App\Models\User;
+>>>>>>> user_credential_privileges Add, Read and Delete
 
 class CredentialController extends Controller
 {
     function notificationPasswordsView() {
         return view('dashboard');
     }
+<<<<<<< HEAD
 
     function allPasswordsView() {
         return view('passwords-view');
     }
+=======
+    
+    function allPasswordsView() {
+        return view('passwords-view');
+    }
+
+    public function getUserCredentials(Request $request) {
+        $validator = Validator::make($request->all(), 
+        [ 
+            'user_id' => 'required',
+        ]);
+
+        $input = $request->only('user_id');
+        $user = User::find($input['user_id']);
+        
+        $credentials = [];
+        foreach ($user->personalCredentialPrivileges as $credential) {
+            $credentials[] = $credential->pivot->credential_id;
+        }
+
+        return $credentials;
+    }
+
+    public function getCredentialUsers(Request $request) {
+        $validator = Validator::make($request->all(), 
+        [ 
+            'credential_id' => 'required',
+        ]);
+
+        $input = $request->only('credential_id');
+        $credential = Credential::find($input['credential_id']);
+
+        $users = [];
+        foreach ($credential->privilegedUsers as $user) {
+            $users[] = $user->pivot->user_id;
+        }
+
+        return $users;
+    }
+
+    public function addUserToCredential(Request $request) {
+        $validator = Validator::make($request->all(), 
+        [ 
+            'user_id' => 'required',
+            'credential_id' => 'required',
+        ]);
+
+        $input = $request->only('user_id', 'credential_id');
+
+        $user = User::find($input['user_id']);
+        $credential = Credential::find($input['credential_id']);
+        
+        $user->personalCredentialPrivileges()->attach($credential);
+    }
+
+    public function removeUserFromCredential(Request $request) {
+        $validator = Validator::make($request->all(), 
+        [ 
+            'user_id' => 'required',
+            'credential_id' => 'required',
+        ]);
+
+        $input = $request->only('user_id', 'credential_id');
+
+        $user = User::find($input['user_id']);
+        $credential = Credential::find($input['credential_id']);
+        
+        $user->personalCredentialPrivileges()->detach($credential);
+    }
+>>>>>>> user_credential_privileges Add, Read and Delete
 }
