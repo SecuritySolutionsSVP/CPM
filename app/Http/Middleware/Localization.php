b3b21963310ceafc9ab4fd46c5c6ed66a561891c
Kristian Lind Kristensen <kris9894@edu.mercantec.dk>
2021-06-04 09:48:13 +0200

    add Localization middleware

---
+++ app/Http/Middleware/Localization.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Auth;
+use PHPUnit\Framework\Constraint\IsNull;
+
+class Localization
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $user = Auth::user();
+        if($user && $user->locale) {
+            App::setLocale($user->locale);
+        }
+        return $next($request);
+    }
+}
