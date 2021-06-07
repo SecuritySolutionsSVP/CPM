1372b2da65d62f8332aaf72c6e2687bbc2093726
Kristian Lind Kristensen <kris9894@edu.mercantec.dk>
2021-06-07 15:07:53 +0200

    Setup data for GroupOverview page

--- app/Http/Livewire/GroupOverview.php
+++ app/Http/Livewire/GroupOverview.php
@@ -7,10 +7,22 @@ use Livewire\Component;
 class GroupOverview extends Component
 {
     public $groups;
+    public $selectedGroup;
     public $showModal = false;
     
     public function render()
     {
         return view('livewire.group-overview');
+    }
+
+    public function editGroupModal($groupId) {
+        $this->selectedGroup = $this->findGroupById($groupId);
+        $this->showModal = true;
+    }
+
+    private function findGroupById($groupId) {
+        return $this->groups->first(function($group) use ($groupId) {
+            return $group->id == $groupId;
+        });
     }
 }
