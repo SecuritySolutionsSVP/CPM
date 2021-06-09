4119097efc79ed1c2f20b2a442ec685722985d95
Kristian Lind Kristensen <kris9894@edu.mercantec.dk>
2021-06-09 23:56:54 +0200

    Add credential creation support to project

--- app/Http/Livewire/PasswordOverview.php
+++ app/Http/Livewire/PasswordOverview.php
@@ -3,7 +3,11 @@
 namespace App\Http\Livewire;
 
 use App\Models\Credential;
+use App\Models\CredentialGroup;
+use App\Models\UserCredentialAccessLog;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class PasswordOverview extends Component
@@ -11,30 +15,66 @@ class PasswordOverview extends Component
     
     public $credentials;
     public $selectedCredential;
+    public $shouldCreateNewCredentialGroup = false;
     
     public $showModal = false;
     public $editModal = false;
     public $createModal = false; 
+    public $success = false;
 
     public function render()
     {
         return view('livewire.password-overview');
     }
 
+    public function createCredential($formData) {
+        $credential = new Credential;
+        $credentialGroup = filter_var($this->shouldCreateNewCredentialGroup, FILTER_VALIDATE_BOOLEAN)
+            ? CredentialGroup::factory()->create(["name" => $formData['asset']])
+            : $this->findCredentialGroupByName($formData['asset']);
+        $credential->credentialGroup()->associate($credentialGroup);
+        $credential->is_sensitive = isset($formData['is_sensitive']) 
+            ? $formData['is_sensitive'] == "on"
+            : false;
+        $credential->username = $formData['username'];
+        $credential->password = $formData['password'];
+        $credential->password_last_updated_at = Carbon::now();
+        $credential->save();
+
+        // Fetch current user
+        $user = Auth::user();
+
+        // Give the user who made the credential access to the credential
+        $credential->privilegedUsers()->attach($user->id);
+
+        // Create an association that the user has read the password.
+        UserCredentialAccessLog::create([
+            'user_id' => $user->id,
+            'credential_id' => $credential->id
+        ]);
+
+        $this->success = true;
+    }
+
+    public function editPassword($formData) {
+        ddd($formData);
+    }
+
     public function displayCredentialAccessModal($credId) {
-        $this->selectedCredential = $this->findCredentialById($credId);
         $this->hideAllModals();
+        $this->selectedCredential = $this->findCredentialById($credId);
         $this->showModal = true;
     }
 
     public function displayCredentialCreateModal() {
         $this->hideAllModals();
+        $this->shouldCreateNewCredentialGroup = "false";
         $this->createModal = true;
     }
 
     public function displayCredentialEditModal($credId) {
-        $this->selectedCredential = $this->findCredentialById($credId);
         $this->hideAllModals();
+        $this->selectedCredential = $this->findCredentialById($credId);
         $this->editModal = true;
     }
 
@@ -43,10 +83,16 @@ class PasswordOverview extends Component
             return $credential->id == $id;
         });
     }
+    private function findCredentialGroupByName($name) {
+        return CredentialGroup::firstWhere('name', $name);
+    }
 
-    private function hideAllModals() {
+    public function hideAllModals() {
         $this->showModal = false;
         $this->editModal = false;
         $this->createModal = false;
+        $this->shouldCreateNewCredentialGroup = "false";
+        $this->selectedCredential = null;
+        $this->success = null;
     }
 }
--- resources/views/livewire/password-overview.blade.php
+++ resources/views/livewire/password-overview.blade.php
@@ -1,5 +1,8 @@
 <div>
     <div class="password-list">
+        <p>
+            <i class="fas fa-plus password-list__list__item__icons__open-settings" wire:click="displayCredentialCreateModal()"></i>
+        </p> 
         <div class="password-list__list">
             @foreach ($credentials as $credential)
                 <div class="password-list__list__item clearfix">
@@ -7,12 +10,12 @@
                         {{ $credential->username }} {{ trans('Passwords for') }} {{ $credential->credentialGroup->name }}
                     </div>
                     <div class="password-list__list__item__created">
-                        <a href="#">{{ $credential->created_at->diffForHumans() }}</a>
+                        {{ $credential->created_at->diffForHumans() }}
                     </div>
                     <div class="password-list__list__item__icons">
-                        <i class="fas fa-user password-list__list__item__icons__get-username"  wire:click="displayCredentialAccessModal({{$credential->id}})"></i>
-                        <i class="fas fa-key password-list__list__item__icons__get-password"></i>
-                        <i class="fas fa-cogs password-list__list__item__icons__open-settings"></i>
+                        <i class="fas fa-user password-list__list__item__icons__get-username" wire:click="displayCredentialAccessModal({{$credential->id}})"></i>
+                        <i class="fas fa-key password-list__list__item__icons__get-password" wire:click="displayCredentialEditModal({{$credential->id}})"></i>
+                        {{-- <i class="fas fa-cogs password-list__list__item__icons__open-settings"></i> --}}
                     </div>
                 </div>
             @endforeach
@@ -21,7 +24,7 @@
     @if ($showModal && $selectedCredential)
         <div class="password-modal">
             <div class="text-right">
-                <i class="fas fa-times fa-2x cursor-pointer" wire:click="$set('showModal', false)"></i>
+                <i class="fas fa-times fa-2x cursor-pointer" wire:click="hideAllModals()"></i>
             </div>
             <div>
                 <h2> {{ trans('Passwords for') }} {{ $selectedCredential->username}}</h2>
@@ -43,10 +46,10 @@
                         </div>
                     @endforeach
                 </div>
-                <div class="mb-12">
+                {{-- <div class="mb-12">
                     <a class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
                         href="#">{{ trans('See more...') }}</a>
-                </div>
+                </div> --}}
                 <div>
                     <button
                         class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">{{ trans('Cancel') }}</button>
@@ -55,8 +58,99 @@
                 </div>
             </div>
         </div>
-        <div class="password-modal__overlay" wire:click="$set('showModal', false)">
+        <div class="password-modal__overlay" wire:click="hideAllModals()">
         </div>
+    @elseif ($editModal)
+    <div class="password-modal">
+        <div class="text-right">
+            <i class="fas fa-times fa-2x cursor-pointer" wire:click="hideAllModals()"></i>
+        </div>
+        <div>
+            <form wire:submit.prevent="editPassword(Object.fromEntries(new FormData($event.target)))"></form>
+                <h1> {{ trans('Change password') }} </h2>
+                <div class="password-modal__credentials">
+                    <p>{{ trans('Disclaimer: Only change this after you\'ve changed the password locally') }}</p>
+                    <p>{{ trans('Asset') }}: {{ $selectedCredential->credentialGroup->name }}
+                    <p>{{ trans('Username') }}: {{ $selectedCredential->username }}</p>
+                    <label for="password">{{ trans('Password') }}</label>
+                    <div>
+                        <input type="password" name="password" placeholder="{{ trans('New password')}}">
+                    </div>
+                </div>
+                <div>
+                    <button type="button" wire:click="hidAllModals()"
+                        class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">{{ trans('Cancel') }}</button>
+                    <button
+                        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">{{ trans('Save') }}</button>
+                </div>
+            </form>
+        </div>
+    </div>
+    <div class="password-modal__overlay" wire:click="hideAllModals()"></div>
+    @elseif ($createModal)
+    <div class="password-modal">
+        <div class="text-right">
+            <i class="fas fa-times fa-2x cursor-pointer" wire:click="hideAllModals()"></i>
+        </div>
+        <div>
+            <form wire:submit.prevent="createCredential(Object.fromEntries(new FormData($event.target)))">
+                <h2> {{ trans('Create new credential') }}</h2>
+                <div class="password-modal__credentials">
+                    <label for="asset">{{ trans('Asset') }}</label>
+                    <div>
+                        <div>
+                            {{-- <label for="newCredentialGroup">{{ trans('Use existing or create new asset') }}</label> --}}
+                            <div>
+                                <input type="radio" name="newCredentialGroup" id="newCredentialGroup_1" wire:model="shouldCreateNewCredentialGroup" wire:key="yes" value="true" /> 
+                                <label for="newCredentialGroup_1">{{ trans('New asset') }}</label>
+                                <input type="radio" name="newCredentialGroup" id="newCredentialGroup_2" wire:model="shouldCreateNewCredentialGroup" wire:key="no" value="false" checked/> 
+                                <label for="newCredentialGroup_2">{{ trans('Existing asset') }}</label>
+                            </div>
+                            <div>
+                                @if (filter_var($shouldCreateNewCredentialGroup, FILTER_VALIDATE_BOOLEAN))
+                                <label for="asset">{{ trans('New asset') }}</label>
+                                <div>
+                                    <input x-show="false" type="text" name="asset" placeholder="{{ trans('New asset') }}">
+                                </div>
+                                @else
+                                <label for="asset">{{ trans('Select existing asset') }}</label>
+                                <div>
+                                    <select name="asset">
+                                        @foreach(App\Models\CredentialGroup::all() as $key => $cGroup)
+                                        <option value="{{$cGroup->name}}" wire:key="{{ $key }}">{{ $cGroup->name }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                                @endif
+                            </div>
+                        </div>
+                    </div>
+                    <label for="username">{{ trans('Username') }}</label>
+                    <div>
+                        <input type="text" name="username">
+                    </div>
+                    <label for="password">{{ trans('Password') }}</label>
+                    <div>
+                        <input type="password" name="password">
+                    </div>
+                    <label for="is_sensitive">{{ trans('Mark as sensitive') }}</label>
+                    <input type="checkbox" name="is_sensitive" id="is_sensitive">
+                </div>
+                <div>
+                    <button type="button" wire:click="hideAllModals()"
+                        class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">{{ trans('Cancel') }}</button>
+                    <button type="submit"
+                        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">{{ trans('Create') }}</button>
+                </div>
+                @if ($success)
+                    <div class="text-green-700">
+                        {{ trans('Successfully created new credential') }}
+                    </div>
+                @endif
+            </form>
+        </div>
+    </div>
+    <div class="password-modal__overlay" wire:click="hideAllModals()"></div>
     @endif
 
 </div>
