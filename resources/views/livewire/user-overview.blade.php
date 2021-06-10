84d25b18ab788498118761ca648e10bca81daddb
Jack Jensen <numtjok1234@gmail.com>
2021-06-10 16:44:59 +0200

    added missing translations

--- resources/views/livewire/user-overview.blade.php
+++ resources/views/livewire/user-overview.blade.php
@@ -23,48 +23,48 @@
             <form wire:submit.prevent="editUser(Object.fromEntries(new FormData($event.target)))">
                 <input name="id" type="hidden" value="{{$selectedUser->id}}">
                 <input name="role_id" type="hidden" value="{{$selectedUser->role_id}}">
-                <label for="first_name">Fornavn</label>
+                <label for="first_name">{{ trans('First name') }}</label>
                 <input name="first_name" type="text" value="{{$selectedUser->first_name}}">
-                <label for="last_name">Efternavn</label>
+                <label for="last_name">{{ trans('Last name') }}</label>
                 <input name="last_name" type="text" value="{{$selectedUser->last_name}}">
-                <label for="email">Email</label>
+                <label for="email">{{ trans('Email') }}</label>
                 <input name="email" type="text" value="{{$selectedUser->email}}">
-                <label for="locale">Sprog</label>
+                <label for="locale">{{ trans('Language') }}</label>
                 <select name="locale">
                     <option value="null" @if ($selectedUser->locale == null)
                         selected='selected'
-                    @endif>Vælg Sprog</option>
+                    @endif>{{ trans('Choose Language') }}</option>
                     <option value="en" @if ($selectedUser->locale == "en")
                         selected='selected'
-                    @endif>Engelsk</option>
+                    @endif>{{ trans('English') }}</option>
                     <option value="da" @if ($selectedUser->locale == "da")
                         selected='selected'
-                    @endif>Dansk</option>
+                    @endif>{{ trans('Danish') }}</option>
                 </select>
-                <a href="#" class="block text-blue-500 hover:underline">Reset Password</a>
+                <a href="#" class="block text-blue-500 hover:underline">{{ trans('Reset Password') }}</a>
                 <br>
-                <div class="mt-3 inline block bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded cursor-pointer" wire:click="deleteUser({{$selectedUser->id}})">Slet Bruger</div>
-                <button class="mt-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Gem Bruger</button>
+                <div class="mt-3 inline block bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded cursor-pointer" wire:click="deleteUser({{$selectedUser->id}})">{{ trans('Delete User') }}</div>
+                <button class="mt-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">{{ trans('Save User') }}</button>
             </form>
 
             @else
                 <form wire:submit.prevent="createUser(Object.fromEntries(new FormData($event.target)))">
                     <input name="role_id" type="hidden" value="2">
-                    <label for="first_name">Fornavn</label>
+                    <label for="first_name">{{ trans('First name') }}</label>
                     <input name="first_name" type="text">
-                    <label for="last_name">Efternavn</label>
+                    <label for="last_name">{{ trans('Last name') }}</label>
                     <input name="last_name" type="text" >
-                    <label for="email">Email</label>
+                    <label for="email">{{ trans('Email') }}</label>
                     <input name="email" type="text">
-                    <label for="locale">Sprog</label>
+                    <label for="locale">{{ trans('Language') }}</label>
                     <select name="locale">
-                        <option value="null" selected='selected'>Vælg Sprog</option>
-                        <option value="en">Engelsk</option>
-                        <option value="da">Dansk</option>
+                        <option value="null" selected='selected'>{{ trans('Choose Language') }}</option>
+                        <option value="en">{{ trans('English') }}</option>
+                        <option value="da">{{ trans('Danish') }}</option>
                     </select>
                     <br>
-                    <button class="mt-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Gem Bruger</button>
-                    <div class="mt-3 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded cursor-pointer inline-block" wire:click="closeModal">Annuller</div>
+                    <button class="mt-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">{{ trans('Save User') }}</button>
+                    <div class="mt-3 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded cursor-pointer inline-block" wire:click="closeModal">{{ trans('Cancel') }}</div>
 
                     </form>
 
