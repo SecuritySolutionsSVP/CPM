f20792d897320ca4009ffb76b899091b31937d46
Kristian Lind Kristensen <kris9894@edu.mercantec.dk>
2021-06-10 16:16:47 +0200

    Prepare for fetching password

--- app/Mail/TwoFactorMail.php
+++ app/Mail/TwoFactorMail.php
@@ -2,6 +2,7 @@
 
 namespace App\Mail;
 
+use App\Models\TwoFactorCredentialToken;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
@@ -12,13 +13,20 @@ class TwoFactorMail extends Mailable
     use Queueable, SerializesModels;
 
     /**
+     * The token instance.
+     * 
+     * @var \App\Models\TwoFactorCredentialToken
+     */
+    protected $token;
+
+    /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(TwoFactorCredentialToken $token)
     {
-        //
+        $this->token = $token;
     }
 
     /**
@@ -28,6 +36,11 @@ class TwoFactorMail extends Mailable
      */
     public function build()
     {
-        return $this->view('mail.two_factor_mail');
+        return $this->view('mail.two_factor_mail')
+                    ->with([
+                        'token' => $this->token->name,
+                        'expiration' => $this->token->expiration,
+                        'credential' => $this->token->credential
+                    ]);
     }
 }
--- resources/views/mail/two_factor_mail.blade.php
+++ resources/views/mail/two_factor_mail.blade.php
@@ -1,1 +1,7 @@
-Test email template
+<div>
+    <p>{{ trans('You have requested access to asset') }}: {{ $credential->credentialGroup->name }}</p>
+    <p>{{ trans('For user') }}: {{ $credential->username }}</p>
+
+    <p>{{ trans('Please enter the following token into your browser') }}:</p>
+    <p>{{ $token }}</p>
+</div>
