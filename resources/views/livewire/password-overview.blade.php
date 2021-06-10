69f7fe0f459f2f72a331bbfbb6233642c10b69e2
Kristian Lind Kristensen <kris9894@edu.mercantec.dk>
2021-06-10 18:31:04 +0200

    Make showPassword show the correct password

--- resources/views/livewire/password-overview.blade.php
+++ resources/views/livewire/password-overview.blade.php
@@ -36,7 +36,7 @@
                         <button>{{ trans('Validate') }}</button>
                     </form>
                     @elseif($showPassword)
-                    {{ $credential->password }}
+                    {{ $selectedCredential->password }}
                     @else
                     <button wire:click="requestAccessToCredential()" 
                         class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
