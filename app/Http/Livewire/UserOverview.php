e6f1c81cec1e1accdb1731c9fc525b71314f0ffc
Jack Jensen <numtjok1234@gmail.com>
2021-06-10 13:06:27 +0200

    Creation and update of users added locale to user creation and updating

--- app/Http/Livewire/UserOverview.php
+++ app/Http/Livewire/UserOverview.php
@@ -3,6 +3,8 @@
 namespace App\Http\Livewire;
 
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 use Livewire\Component;
 
 class UserOverview extends Component
@@ -17,30 +19,61 @@ class UserOverview extends Component
         return view('livewire.user-overview');
     }
 
-    public function closeModal(){
-        $this->showModal=false;
-        $this->selectedUser='';
+    public function closeModal()
+    {
+        $this->showModal = false;
+        $this->selectedUser = '';
     }
 
-    public function createUser(){
-        
+    public function createUser($formData)
+    {
+        $password = Hash::make(Str::random(8));
+        $request = [
+            'first_name' => $formData['first_name'],
+            'last_name' => $formData['last_name'],
+            'role_id' => $formData['role_id'],
+            'email' => $formData['email'],
+            'password' => $password,
+            'c_password' => $password,
+            'locale' => $formData['locale']
+        ];
+        $user = new User();
+        $user->create($request);
+        return redirect('/users');
     }
 
-    public function deleteUser($userId){
+    public function editUser($formData)
+    {
+        $request = [
+            'id' => $formData['id'],
+            'first_name' => $formData['first_name'],
+            'last_name' => $formData['last_name'],
+            'role_id' => $formData['role_id'],
+            'email' => $formData['email'],
+            'locale' => $formData['locale'],
+        ];
+        $user = $this->findUserById($formData['id']);
+        $user->update($request);
+        return redirect('/users');
+    }
+
+    public function deleteUser($userId)
+    {
         $user = $this->findUserById($userId);
         $user->delete();
+        return redirect('/users');
     }
 
-    public function editUserModal($userId) {
+    public function editUserModal($userId)
+    {
         $this->selectedUser = $this->findUserById($userId);
         $this->showModal = true;
     }
 
-    private function findUserById($userId) {
-        return $this->users->first(function($user) use ($userId) {
+    private function findUserById($userId)
+    {
+        return $this->users->first(function ($user) use ($userId) {
             return $user->id == $userId;
         });
     }
-
-
 }
